ede7ac25e079e77f87f97c99e283dd5b4004dc36
liusxs <1694181697@qq.com>
2022-11-23 14:59:37 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <div id="app">
-    
-  </div>
+  <router-view></router-view>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      
-    }
-  }
-}
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+import { useRepositoryStore } from './store/repository';
+
+export default defineComponent({
+  name: 'App'
+});
+</script>
+<script setup lang="ts">
+onMounted(()=>{
+  useRepositoryStore().getGithubData();
+  useRepositoryStore().getGiteebData();
+})
 </script>
 
-<style lang="scss">
-
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+}
+.button{
+  cursor: pointer;
+}
+.default-transition{
+  transition: all 0.4s;
+}
 </style>
